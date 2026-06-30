c2e01a2fcf20e33753f43e63cf81af20e87c1ad1
ShamimSB <bhuiyanshamim09@gmail.com>
2026-06-30 17:09:04 +0600

    added firebase

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,38 @@
 // import React from 'react';
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { FaBars } from "react-icons/fa";
 import { FaRegWindowClose } from "react-icons/fa";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+import reactLogo from '../assets/react-removebg-preview.png';
 
 
 const Navbar = () => {
 
 const [isOpen,setIsOpen] = useState(false);
 
+const [activeLink,setActiveLink] = useState("/");
+
+const location = useLocation();
+
+useEffect(()=>{
+  setActiveLink(location.pathname || "/")
+},[location.pathname])
+
+
+
+
 const toggleMenu = ()=>{
   setIsOpen(!isOpen);
+}
+
+const handleLinkClick = (path)=>{
+  setActiveLink(path);
 }
 
   return (
     <nav className="bg-[#38ada9] text-white px-4 py-4 md:py-6 sticky top-0 border-b-2 z-10 border-white" >
       <div className=" container mx-auto flex justify-between items-center">
-          <h2 className="text-xl font-bold">React practice</h2>
+          <img src={reactLogo} alt="React Logo" className="h-18 w-auto object-contain rounded-md" />
 
           {/* Mobile menu bar */}
           <div className="md:hidden">
@@ -30,11 +46,25 @@ const toggleMenu = ()=>{
           </div>
           
           <ul className="hidden md:flex space-x-4 md:space-x-6">
-            <li> <Link to="/" className="hover:text-green-700">Home</Link> </li>
-            <li> <Link to="/products" className="hover:text-green-700">Products</Link> </li>
-            <li> <Link to="/blogs" className="hover:text-green-700">Blogs</Link> </li>
-            <li> <Link to="/contact" className="hover:text-green-700">Contact</Link> </li>
-            <li> <Link to="/about" className="hover:text-green-700">About</Link> </li>
+            <li> 
+              <Link to="/" onClick={()=> handleLinkClick("/")} className={`${activeLink === "/" ? "text-red-600" : "hover:text-green-400"}`}>Home</Link> 
+            </li>
+
+            <li> 
+              <Link to="/products" onClick={()=> handleLinkClick("/products")} className={`${activeLink === "/products" ? "text-red-600" : "hover:text-green-400"}`}>Products</Link>
+            </li>
+
+            <li> 
+              <Link to="/blogs" onClick={()=> handleLinkClick("/blogs")}  className={`${activeLink === "/blogs" ? "text-red-600" : "hover:text-green-600" }`}>Blogs</Link> 
+            </li>
+
+            <li> 
+              <Link to="/contact" onClick={()=> handleLinkClick("/contact")} className={`${activeLink === "/contact" ? "text-red-600" : "hover:text-green-700"}`}>Contact</Link> 
+            </li>
+
+            <li> 
+              <Link to="/about" onClick={()=> handleLinkClick("/about")} className={`${activeLink === "/about" ? "text-red-600" : "hover:text-green-700"}`} >About</Link> 
+            </li>
           </ul>
 
           <button className="hidden md:block bg-white text-black px-4 py-2 rounded cursor-pointer hover:bg-sky-100">Login</button>
